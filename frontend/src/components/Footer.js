83f1dc83dfdac33a06ceb74e605dc5097fdb6b1f
Vatsal-Thapliyal <vatsalthapliyal4178@gmail.com>
2024-03-20 17:07:30 +0530

    HomePage - 20 March 2024

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -14,17 +14,19 @@ function Footer() {
         Connect with qualified professionals, receive timely medical advice, and take control of your well-being effortlessly. 
         Your health is our priority, and at WellMed, we're dedicated to providing convenient, reliable healthcare solutions tailored to your needs.</p>
       </div>
-      <div>
-        <div className='d-flex justify-content-center align-items-center'>
-          <img src={gmailIcon} alt="Gmail" className='icons' />
-          <img src={instagramIcon} alt="Instagram" className='icons'/>
-          <img src={facebookIcon} alt="Facebook" className='icons'/>
-          <img src={twitterIcon} alt="Twitter" className='icons'/>
-        </div>
+      <div className='text-center'>
+        <p className='text-light text-sm'>
+          Get connected with us on social networks:
+        </p>
       </div>
-
       <div className='d-flex justify-content-center align-items-center'>
-        <p className='text-sm text-light m-2'>&#169; 2024 Copyright : WellMed</p>
+        <img src={gmailIcon} alt="Gmail" className='icons' />
+        <img src={instagramIcon} alt="Instagram" className='icons'/>
+        <img src={facebookIcon} alt="Facebook" className='icons'/>
+        <img src={twitterIcon} alt="Twitter" className='icons'/>
+      </div>
+      <div className='d-flex justify-content-center align-items-center'>
+        <p className='text-sm text-light m-3'>&#169; 2024 Copyright : WellMed</p>
       </div>
     </div>
 
